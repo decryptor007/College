7c8713d2ec2be3073679d224fe2a4fcb9001e9e4
decryptor007 <smazen98@gmail.com>
2020-12-22 16:28:05 +0200

    comments and formatting

--- decryptoricDS.dart
+++ decryptoricDS.dart
@@ -1,39 +1,47 @@
-class queue<T>{
-  var myQueue=<T>[];
-  void push(x){
+// Mazen Soliman Youssef aka decryptor007 , 20171454959
+
+class queue<T> {
+  var myQueue = <T>[];
+  void push(x) {
     myQueue.add(x);
   }
-  T pop(){
+
+  T pop() {
     return myQueue.removeAt(0);
   }
-  T peek(){
+
+  T peek() {
     return myQueue[0];
   }
-  bool isEmpty(){
-    print(myQueue.length);
-    return myQueue.length==0;
+
+  bool isEmpty() {
+    return myQueue.length == 0;
   }
-  int getLength(){
+
+  int getLength() {
     return myQueue.length;
   }
 }
-class stack<T>{
-  var myStack=<T>[];
-  void push(x){
+
+class stack<T> {
+  var myStack = <T>[];
+  void push(x) {
     myStack.add(x);
   }
-  T pop(){
-    return(myStack.removeLast());
+
+  T pop() {
+    return (myStack.removeLast());
   }
-  T peek(){
-    return myStack[myStack.length-1];
+
+  T peek() {
+    return myStack[myStack.length - 1];
   }
-  bool isEmpty(){
-    print(myStack.length);
-    return myStack.length==0;
+
+  bool isEmpty() {
+    return myStack.length == 0;
   }
-  int getLength(){
+
+  int getLength() {
     return myStack.length;
   }
 }
-
--- tests.dart
+++ tests.dart
@@ -1,79 +1,85 @@
 import 'package:test/test.dart';
 import 'dart:math';
 import 'decryptoricDS.dart';
+// Mazen Soliman Youssef aka decryptor007 , 20171454959
 void main() {
-var s;
-var q;
-var r=Random();
-print(r.nextInt(1000000000));
-setUp((){
-s=new stack<int>();
-q=new queue<int>();
-});
-group('stack',() {
-  test('stack push and pop', () {
-    for(var i=0;i<10000;i++){
-    	s.push(r.nextInt(10000000));
-    	expect(s.getLength(),i+1);
-    }
-    for(var i=1;i<10000;i++){
-    	s.pop();
-    	expect(s.getLength(),9999-i+1);
-    }
+  var s;
+  var q;
+  var r = Random(); // random class usage as your hint suggested :"
+
+  setUp(() {
+    //executed once before every test.
+    s = new stack<int>();
+    q = new queue<int>();
   });
+  //first group for stack testing
+  group('stack', () {
+    test('stack push and pop', () {
+      //testing for 10000 push and pop
+      for (var i = 0; i < 100000; i++) {
+        s.push(r.nextInt(1000000));
+        expect(s.getLength(), i + 1);
+      }
+      for (var i = 1; i < 100000; i++) {
+        s.pop();
+        expect(s.getLength(), 99999 - i + 1);
+      }
+    });
 
-  test('stack pop order test', () {
-    s.push(1);
-    s.push(2);
-    expect(s.pop(),2);
+    test('stack pop order test', () {
+      s.push(1);
+      s.push(2);
+      expect(s.pop(), 2);
+    });
+
+    test('stack peek function', () {
+      s.push(1);
+      s.push(2);
+      s.push(3);
+      expect(s.peek(), 3);
+    });
+
+    test('stack isEmptyTest', () {
+      expect(s.isEmpty(), true);
+      s.push(1);
+      expect(s.isEmpty(), false);
+    });
   });
- 
-test('stack peek function',() {
-	s.push(1);
-	s.push(2);
-	s.push(3);
-	expect(s.peek(),3);
-});
-test('stack isEmptyTest',() {
-	expect(s.isEmpty(),true);
-	s.push(1);
-	expect(s.isEmpty(),false);
-});
+  //first group for queue testing (same code t2ribn except behaviour differences)
+  group('queue', () {
+    test('queue push and pop', () {
+      for (var i = 0; i < 100000; i++) {
+        q.push(r.nextInt(1000000));
+        expect(q.getLength(), i + 1);
+      }
+      for (var i = 1; i < 100000; i++) {
+        q.pop();
+        expect(q.getLength(), 99999 - i + 1);
+      }
+    });
 
-});
+    test('queue pop order test', () {
+      q.push(1);
+      q.push(2);
+      expect(q.pop(), 1);
+    });
 
-group('queue',() {
-  test('queue push and pop', () {
-    for(var i=0;i<10000;i++){
-    	q.push(r.nextInt(10000000));
-    	expect(q.getLength(),i+1);
-    }
-    for(var i=1;i<10000;i++){
-    	q.pop();
-    	expect(q.getLength(),9999-i+1);
-    }
+    test('queue peek function', () {
+      q.push(1);
+      q.push(2);
+      q.push(3);
+      expect(q.peek(), 1);
+    });
+    test('queue isEmptyTest', () {
+      expect(q.isEmpty(), true);
+      q.push(1);
+      expect(q.isEmpty(), false);
+    });
   });
-
-  test('queue pop order test', () {
-    q.push(1);
-    q.push(2);
-    expect(q.pop(),1);
+  
+  tearDown(() {
+    //runs after every test
+    s = null;
+    q = null;
   });
-
-test('queue peek function',() {
-	q.push(1);
-	q.push(2);
-	q.push(3);
-	expect(q.peek(),1);
-});
-test('queue isEmptyTest',() {
-	expect(q.isEmpty(),true);
-	q.push(1);
-	expect(q.isEmpty(),false);
-});
-});
-tearDown((){
-s=null;
-q=null;
-});
 }
